5b289aed3d186d1c70e3025875bcaf9b560ea373
venkatapexon <venkat.velagala@apexon.com>
2024-02-11 09:41:33 -0600

    logging configuration added

--- logger/log_config.py
+++ logger/log_config.py
@@ -2,5 +2,29 @@ import logging.config
 
 logger = logging.getLogger("myapp")
 # Here try to get name of the ml model we have.
-import os
-print(os.path.basename(os.getcwd()))
+
+
+logging_config = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard',
+            'level': 'DEBUG',
+            'stream': 'ext://sys.stdout',  # Use standard output
+        },
+    },
+    'loggers': {
+        '': {  # root logger
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    }
+}
